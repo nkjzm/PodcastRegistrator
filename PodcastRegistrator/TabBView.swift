d79b3db58d9efaa6ecf59d0c947ddc8d38707f2b
Nakaji Kohki <boomerang.cube@gmail.com>
2021-02-27 09:39:33 +0900

    タブ化

---
+++ PodcastRegistrator/TabBView.swift
@@ -0,0 +1,22 @@
+//
+//  TabBView.swift
+//  PodcastRegistrator
+//
+//  Created by 中地功貴 on 2021/02/27.
+//  Copyright © 2021 Nakaji Kohki. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct TabBView: View {
+    var body: some View {
+        Text("This is Tab B.")
+    }
+}
+
+struct TabBView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBView()
+    }
+}
