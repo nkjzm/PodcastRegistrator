c7b07fab0b02026fd4f9c47c60617d913012fde0
Nakaji Kohki <boomerang.cube@gmail.com>
2022-03-01 07:36:38 +0900

    feat: 色々ハードコード

--- PodcastRegistrator/TabBView.swift
+++ PodcastRegistrator/TabBView.swift
@@ -11,11 +11,11 @@ import SwiftUI
 
 struct TabBView: View {
     
-    @State private var number : String  = ""
+    // @State private var number : String  = ""
     @State private var audioPath : String = "未選択"
     @State private var progress : String = "処理開始前"
     @State private var enableSavingPreConvertedFile = true
-    @State private var enableOptimization = false
+    // @State private var enableOptimization = false
     @State private var valueAmount : Float = -1;
     
     let gitRootPath : String = "/Users/nkjzm/Projects/xrfm.github.io"
@@ -23,31 +23,65 @@ struct TabBView: View {
     let rawAudioRootPath : String = "/Users/nkjzm/Projects/xrfm.github.io/audio/raw"
     let mdRootPath : String = "/Users/nkjzm/Projects/xrfm.github.io/_posts"
     let artworkPath : String = "/Users/nkjzm/Dropbox/xrpodcast.png"
-
+    
     func Convert(callback: @escaping () -> Void) -> Void {
         
         progress = "変換処理開始"
-        self.valueAmount = 20
+        self.valueAmount = 0
         
-        let original = "\"\(audioPath)\""
         
         if enableSavingPreConvertedFile
         {
-            let outputFilePath = "\(self.rawAudioRootPath)/\(TabBView.GetRawAudioName(number: self.number))"
-            self.ConvertToMp3(path: original, outputFilePath: outputFilePath) {
-                progress = "最適化前のファイルの保存が完了"
-                self.valueAmount += 20
+            
+            let files =            getFileInfoListInDir("\(self.rawAudioRootPath)/wav")
+            
+            let rate : Float = 100.0 / Float(files.count)
+            
+            for (i,file) in files.enumerated() {
+                
+                if(file == ".DS_Store")
+                {
+                    continue
+                }
+                
+                let number = file.replacingOccurrences(of: ".wav", with: "")
+                
+                
+                let original = "\(self.rawAudioRootPath)/wav/\(number).wav"
+                
+                let outputFilePath = "\(self.rawAudioRootPath)/\(TabBView.GetRawAudioName(number: number))"
+                
+                progress = "変換開始: " + number
+                
+                self.ConvertToMp3(path: original, outputFilePath: outputFilePath) {
+                    progress = "変換完了: " + number
+                    self.valueAmount += rate
+                }
             }
-        }
-        
-        if enableOptimization
-        {
+            
+            return;
+            
             
         }
         
+        //        if enableOptimization
+        //        {
+        //        }
+        
         callback()
     }
-
+    
+    func getFileInfoListInDir(_ dirName: String) -> [String] {
+        let fileManager = FileManager.default
+        var files: [String] = []
+        do {
+            files = try fileManager.contentsOfDirectory(atPath: dirName)
+        } catch {
+            return files
+        }
+        return files
+    }
+    
     // wavファイルを変換してアートワークを設定する
     func ConvertToMp3(path : String, outputFilePath : String, callback: @escaping () -> Void) -> Void {
         
@@ -69,7 +103,7 @@ struct TabBView: View {
         let episodeNumber = Int(number)!
         return "xrfm_\(String(format: "%03d", episodeNumber)).mp3"
     }
-
+    
     static func GetRawAudioName(number : String) -> String {
         let episodeNumber = Int(number)!
         return "xrfm_\(String(format: "%03d", episodeNumber))_raw.mp3"
@@ -89,19 +123,19 @@ struct TabBView: View {
                     Text("オーディオファイルを開く")
                 }
             }
-            HStack(alignment: .center){
-                Text("回数").frame(width: 100)
-                TextField("0", text: $number)
-            }
+            //            HStack(alignment: .center){
+            //                Text("回数").frame(width: 100)
+            //                TextField("0", text: $number)
+            //            }
             HStack(alignment: .center){
                 Text("変換オプション").frame(width: 100)
                 VStack(alignment: HorizontalAlignment.leading){
                     Toggle(isOn: $enableSavingPreConvertedFile) {
                         Text("最適化前のファイルを保存する")
                     }.disabled(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
-                    Toggle(isOn: $enableOptimization) {
-                        Text("最適化処理を有効にする")
-                    }
+                    //                    Toggle(isOn: $enableOptimization) {
+                    //                        Text("最適化処理を有効にする")
+                    //                    }
                 }
                 Spacer()
             }
